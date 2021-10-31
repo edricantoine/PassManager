3a02586ffd31d1050c8d93fa4bc71a3a02537c4f
Edric Antoine <edric.antoine@gmail.com>
2021-10-30 20:03:10 -0700

    Added GUI for saving to JSON, as well as displaying no. of entries

--- src/main/ui/PassManagerAppGui.java
+++ src/main/ui/PassManagerAppGui.java
@@ -21,18 +21,18 @@ import java.io.IOException;
 //This class references code from the ListDemoProject repo.
 //Link: https://docs.oracle.com/javase/tutorial/uiswing/examples/components/index.html
 
-//GUI for password manager project
+//GUI for password manager project. Represents the "main" panel.
 
 
 public class PassManagerAppGui implements ListSelectionListener {
+    private JFrame frame;
     private static final String JSON_LOC = "./data/pmanager.json";
     private PassManager manager;
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
-    private static final int WIDTH = 800;
-    private static final int HEIGHT = 800;
     private JButton addEntryButton;
     private JButton removeEntryButton;
+    private JButton saveButton;
     private JCheckBox importantBox;
     private JTextField labelField;
     private JTextField usernameField;
@@ -44,10 +44,13 @@ public class PassManagerAppGui implements ListSelectionListener {
     private AddListener addListener;
     private JComboBox<String> categorySelection;
     private String[] catStrings;
+    private JLabel welcomeLabel;
 
 
 
     public PassManagerAppGui() {
+        jsonWriter = new JsonWriter(JSON_LOC);
+        jsonReader = new JsonReader(JSON_LOC);
         catStrings = new String[]{"WORK", "ENTERTAINMENT", "FINANCE", "DEVICES", "OTHER"};
         mainPanel = new JPanel();
         manager = new PassManager();
@@ -58,6 +61,8 @@ public class PassManagerAppGui implements ListSelectionListener {
         entryList.addListSelectionListener(this);
         entryList.setVisibleRowCount(5);
         listScrollPane = new JScrollPane(entryList);
+        loadPassManager();
+        pmToList();
         setUpButtonsAndFields();
         setUpMiscItems();
         setUpPane();
@@ -70,6 +75,11 @@ public class PassManagerAppGui implements ListSelectionListener {
     public void setUpMiscItems() {
         importantBox = new JCheckBox("Make entry important");
         categorySelection = new JComboBox<>(catStrings);
+        saveButton = new JButton("Save");
+        saveButton.addActionListener(new SaveListener());
+        saveButton.setActionCommand("Save");
+        welcomeLabel = new JLabel("Welcome! You currently have " + manager.getNumEntries()
+                + " entry/entries.");
 
 
     }
@@ -102,7 +112,8 @@ public class PassManagerAppGui implements ListSelectionListener {
 
     public void setUpPane() {
         JPanel mainPane = new JPanel();
-        mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.LINE_AXIS));
+        mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.Y_AXIS));
+        mainPane.add(welcomeLabel);
         mainPane.add(labelField);
         mainPane.add(usernameField);
         mainPane.add(passwordField);
@@ -113,22 +124,38 @@ public class PassManagerAppGui implements ListSelectionListener {
         mainPane.add(new JSeparator(SwingConstants.VERTICAL));
         mainPane.add(Box.createHorizontalStrut(3));
         mainPane.add(removeEntryButton);
+        mainPane.add(saveButton);
         mainPane.setOpaque(true);
         mainPanel.add(listScrollPane, BorderLayout.CENTER);
         mainPanel.add(mainPane, BorderLayout.PAGE_END);
 
     }
 
+    //This method references code from the JsonSerializationDemo repo.
+    //Link: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+    //MODIFIES: this
+    //EFFECTS: Reads data from file
+    private void loadPassManager() {
+        try {
+            manager = jsonReader.read();
+            System.out.println("Successfully loaded data from" + JSON_LOC);
+        } catch (IOException e) {
+            System.out.println("Error reading from " + JSON_LOC
+                    + ". Ignore message if this is first usage of application.");
+        }
+    }
+
 
 
     private void pmToList() {
         for (Entry e : manager.getEntries()) {
-            listModel.insertElementAt(e.toString(), 0);
+            listModel.insertElementAt(e.entryString(), 0);
         }
     }
 
     private void displayGUI() {
-        JFrame frame = new JFrame("Password Manager");
+        frame = new JFrame("Password Manager");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         mainPanel.setOpaque(true);
@@ -161,6 +188,7 @@ public class PassManagerAppGui implements ListSelectionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+
             String label = labelField.getText();
             String username = usernameField.getText();
             String password = passwordField.getText();
@@ -180,7 +208,8 @@ public class PassManagerAppGui implements ListSelectionListener {
 
                 manager.addEntry(newE);
                 listModel.insertElementAt(newE.entryString(), index);
-
+                welcomeLabel.setText("Welcome! You currently have " + manager.getNumEntries() + " entry/entries.");
+                frame.pack();
                 entryList.setSelectedIndex(index);
                 entryList.ensureIndexIsVisible(index);
 
@@ -223,6 +252,9 @@ public class PassManagerAppGui implements ListSelectionListener {
             manager.removeEntry(theEntry);
             int index = entryList.getSelectedIndex();
             listModel.remove(index);
+            welcomeLabel.setText("Welcome! You currently have " + manager.getNumEntries()
+                    + " entry/entries.");
+            frame.pack();
 
 
             int size = listModel.getSize();
@@ -242,6 +274,22 @@ public class PassManagerAppGui implements ListSelectionListener {
 
         }
 
+    }
+
+    public class SaveListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            try {
+                jsonWriter.openFile();
+                jsonWriter.write(manager);
+                jsonWriter.close();
+            } catch (FileNotFoundException f) {
+                Toolkit.getDefaultToolkit().beep();
+            }
+
+        }
     }
 
 
